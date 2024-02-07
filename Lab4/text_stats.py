0c3411ae844da62bdac851ab233bee84327991a8
Siddhesh Sreedar <siddheshraw@gmail.com>
2024-02-07 11:13:15 +0100

    hsdakjfhajksd

--- Lab4/text_stats.py
+++ Lab4/text_stats.py
@@ -4,7 +4,8 @@ from collections import Counter
 
 
 def prep_text(f):
-    f = f.replace("\n", " ").replace("\t", " ").replace("_", "").replace("--", " ").lower()
+    f = f.replace("\n", " ").replace("\t", " ").replace(
+        "_", "").replace("--", " ").lower()
 
     return f
 
@@ -16,9 +17,11 @@ def get_letter_count(f):
     # Make sure all letters are lowercase and that there aren't any newlines or tabs
     f = prep_text(f)
 
-    # Count per alphabetic letter    
-    letter_count = {letter: f.count(letter) for letter in "abcdefghijklmnopqrstuvwxyz"}    
-    letter_count = sorted(letter_count.items(), key=lambda x: x[1], reverse=True)
+    # Count per alphabetic letter
+    letter_count = {letter: f.count(letter)
+                    for letter in "abcdefghijklmnopqrstuvwxyz"}
+    letter_count = sorted(letter_count.items(),
+                          key=lambda x: x[1], reverse=True)
 
     return letter_count
 
@@ -29,11 +32,12 @@ def get_word_list(f):
     """
     # Make sure all letters are lowercase and that there aren't any newlines or tabs
     f = prep_text(f)
-    
+
     # Word count
     word_list = f.split(" ")
-    word_list = [word.replace("”", "").replace("“", "").rstrip("'").lstrip("'").rstrip(":").rstrip(";").rstrip("!").rstrip("?").rstrip(")").lstrip("(").rstrip(".").rstrip(",") for word in word_list if word not in ["", " "] and not any(char.isdigit() for char in word)]
-    
+    word_list = [word.replace("”", "").replace("“", "").rstrip("'").lstrip("'").rstrip(":").rstrip(";").rstrip("!").rstrip("?").rstrip(
+        ")").lstrip("(").rstrip(".").rstrip(",") for word in word_list if word not in ["", " "] and not any(char.isdigit() for char in word)]
+
     return word_list
 
 
@@ -51,17 +55,22 @@ def get_most_frequent_words(f):
     Takes a read text file and returns a dictionary of the most frequent words with their most frequent subsequent words
     """
     word_list = get_word_list(f)
-    
+
     word_counts = dict(Counter(word_list))
-    word_counts = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)[:5]
-    
+    word_counts = sorted(word_counts.items(),
+                         key=lambda x: x[1], reverse=True)[:5]
+
     following_word_counts = {}
     for this_word, this_count in word_counts:
-        following_this_word_ind = [ind + 1 for ind, word in enumerate(word_list) if word == this_word]
-        following_this_word_counts = dict(Counter([word_list[ind] for ind in following_this_word_ind]))
-        following_this_word_counts = sorted(following_this_word_counts.items(), key=lambda x: x[1], reverse=True)[:3]
+        following_this_word_ind = [
+            ind + 1 for ind, word in enumerate(word_list) if word == this_word]
+        following_this_word_counts = dict(
+            Counter([word_list[ind] for ind in following_this_word_ind]))
+        following_this_word_counts = sorted(
+            following_this_word_counts.items(), key=lambda x: x[1], reverse=True)[:3]
 
-        following_word_counts[this_word] = (this_count, following_this_word_counts)
+        following_word_counts[this_word] = (
+            this_count, following_this_word_counts)
 
     return following_word_counts
 
@@ -73,12 +82,13 @@ def text_summary(f, file=None):
     if file is not None:
         file = open(file, "a")
 
-    letter_count = get_letter_count(f) # Count per alphabetic letter
+    letter_count = get_letter_count(f)  # Count per alphabetic letter
     for letter, count in letter_count:
         print(f"Count of {letter}: {count}", file=file)
 
-    n_words, n_unique_words = get_word_counts(f) # Word count
-    print(f"Number of words: {n_words}\nNumber of unique words: {n_unique_words}", file=file)
+    n_words, n_unique_words = get_word_counts(f)  # Word count
+    print(f"Number of words: {n_words}\nNumber of unique words: {
+          n_unique_words}", file=file)
 
     following_word_counts = get_most_frequent_words(f)
     for word, info in following_word_counts.items():
@@ -91,24 +101,28 @@ def text_summary(f, file=None):
 if __name__ == "__main__":
     args = sys.argv
 
+    print(args)
+
     if len(args) > 2:
         raise Exception("You may only pass at most two arguments")
     elif len(args) < 1:
         raise Exception("You must pass at least one file to read")
-    
-    if not args[0].endswith(".txt"):
+
+    if not args[1].endswith(".txt"):
         raise Exception("The first argument passed must be a text file.")
 
-    if len(args == 2):
-        if not args[1].endswith(".txt"):
+    if len(args) == 3:
+        if not args[2].endswith(".txt"):
             raise Exception("File to save to must be a text file!")
-        
-        write_bool = 1
 
-    with open(args[0], encoding="utf-8") as file:
+        write_bool = 1
+    else:
+        write_bool = 0
+
+    with open(args[1], encoding="utf-8") as file:
         f = file.read()
-        
+
         if write_bool:
-            text_summary(f, file=args[1])
+            text_summary(f, file=args[2])
         else:
             text_summary(f)
