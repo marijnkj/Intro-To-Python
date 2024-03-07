fc0a9f2266feaf7d99600ccf4efdeb937c7fef49
marijnkj <116631228+marijnkj@users.noreply.github.com>
2024-03-07 14:48:21 +0100

    Think it should work, but running is taking way too long rn

--- Lab4/generate_text.py
+++ Lab4/generate_text.py
@@ -1,46 +1,42 @@
 import sys
 from collections import Counter
+import numpy as np
 import text_stats as ts
-
-
-def get_following_probs(f, word):
-    """
-    Takes a read text file and returns a dictionary of the most frequent words with the probablity of subsequent words
-    """
-    word_list = ts.get_word_list(f)    
-    
-    following_word_ind = [ind + 1 for ind, this_word in enumerate(word_list) if this_word == word]
-    following_word_counts = dict(Counter([word_list[ind] for ind in following_word_ind if ind < len(word_list)]))
-    following_word_props = {word: count / sum(following_word_counts.values()) for word, count in following_word_counts.items()}
-    following_word_props = sorted(following_word_props.items(), key=lambda x: x[1], reverse=True)
-    
-
-    return following_word_props[0][0]
 
 
 if __name__ == "__main__":
     args = sys.argv
 
     if len(args) != 4:
-        raise Exception("Three variables must be passed! A file to open, a starting word, and the max number of words")
+        raise Exception("ERROR: Three variables must be passed! A file to open, a starting word, and the max number of words")
     
     if not args[1].endswith(".txt"):
-        raise Exception("The first argument passed must be a text file.")
+        raise Exception("ERROR: The first argument passed must be a text file.")
     
-    if not isinstance(args[2], str) or not isinstance(args[3], int):
-        raise Exception("The starting word must be a string and the max number of words an integer")
+    if not isinstance(args[2], str):
+        raise Exception("ERROR: The starting word must be a string and the max number of words an integer")
     
     file_name = args[1]
     starting_word = args[2]
-    max_words = args[3]
+    max_words = int(args[3])
     stop = 0
 
     word_list = [starting_word]
+
+    # Error message if file does not exist
+    try:
+        f = open(file_name)
+        f.close()
+    except FileNotFoundError:
+        print("ERROR: This file does not exist!")
+
     with open(file_name, encoding="utf-8") as file:
         f = file.read()
-
+        
         while not stop:
-            word_list.append(get_following_probs(f, word_list[-1]))
+            print(word_list[-1])
+            words_following_this_word, probs_following_this_word = ts.get_following_word_probs(word_list[-1])
+            word_list.append(np.random.choice(words_following_this_word, p=probs_following_this_word))
             
             if len(word_list) == max_words:
                 stop = 1
--- Lab4/text_stats.py
+++ Lab4/text_stats.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 from collections import Counter
+import json
 
 
 def prep_text(f):
@@ -46,69 +47,101 @@ def get_word_counts(f):
     return len(word_list), len(set(word_list))
 
 
-def get_most_frequent_words(f):
+def get_most_frequent_words(f, save=False):
     """
     Takes a read text file and returns a dictionary of the most frequent words with their most frequent subsequent words
     """
     word_list = get_word_list(f)
     
     word_counts = dict(Counter(word_list))
-    word_counts = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)[:5]
+    word_counts = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)
     
     following_word_counts = {}
     for this_word, this_count in word_counts:
         following_this_word_ind = [ind + 1 for ind, word in enumerate(word_list) if word == this_word]
-        following_this_word_counts = dict(Counter([word_list[ind] for ind in following_this_word_ind]))
-        following_this_word_counts = sorted(following_this_word_counts.items(), key=lambda x: x[1], reverse=True)[:3]
+        following_this_word_counts = dict(Counter([word_list[ind] for ind in following_this_word_ind if ind < len(word_list)]))
+        following_this_word_counts = sorted(following_this_word_counts.items(), key=lambda x: x[1], reverse=True)
 
         following_word_counts[this_word] = (this_count, following_this_word_counts)
 
-    return following_word_counts
+    if save:
+        output_file = open("Lab4/following_word_counts.json", "w")
+        json.dump(following_word_counts, output_file) 
+        output_file.close()
+    else:
+        return following_word_counts
 
 
-def text_summary(f, file=None):
+def get_following_word_probs(word):
+    dict_f = open("following_word_counts.json")
+    following_word_counts = json.load(dict_f)
+
+    following_this_word_counts = following_word_counts[word][1]
+    n_words_following_this_word = sum([count for word, count in following_this_word_counts])
+    words_following_this_word = [word for word, count in following_this_word_counts]
+    probs_following_this_word = [count / n_words_following_this_word for word, count in following_this_word_counts]
+
+    return words_following_this_word, probs_following_this_word
+
+
+def text_summary(f, output_file=None):
     """
     Takes a read text file and prints all the summary stats
     """
-    if file is not None:
-        file = open(file, "a")
+    if output_file is not None:
+        output_file = open(output_file, "a")
 
     letter_count = get_letter_count(f) # Count per alphabetic letter
     for letter, count in letter_count:
-        print(f"Count of {letter}: {count}", file=file)
+        print(f"Count of {letter}: {count}", file=output_file)
 
     n_words, n_unique_words = get_word_counts(f) # Word count
-    print(f"Number of words: {n_words}\nNumber of unique words: {n_unique_words}", file=file)
+    print(f"Number of words: {n_words}\nNumber of unique words: {n_unique_words}", file=output_file)
 
-    following_word_counts = get_most_frequent_words(f)
+    following_word_counts = json.load("following_word_counts.json")
+    following_word_counts = {word: following_word_counts[word][:3] for word in list(following_word_counts)[:5]}
     for word, info in following_word_counts.items():
-        print(f"{word} ({info[0]} occurences)", file=file)
+        print(f"{word} ({info[0]} occurences)", file=output_file)
 
         for following_word, count in info[1]:
-            print(f"--{following_word}, {count}", file=file)
+            print(f"--{following_word}, {count}", file=output_file)
+
+    try:
+        output_file.close()
+    except:
+        pass
 
 
 if __name__ == "__main__":
     args = sys.argv
+    write_bool = 0
 
-    if len(args) > 2:
+    if len(args) > 3:
         raise Exception("You may only pass at most two arguments")
-    elif len(args) < 1:
+    elif len(args) < 2:
         raise Exception("You must pass at least one file to read")
-    
-    if not args[0].endswith(".txt"):
+    if not args[1].endswith(".txt"):
         raise Exception("The first argument passed must be a text file.")
-
-    if len(args == 2):
-        if not args[1].endswith(".txt"):
+    if len(args) == 3:
+        if not args[2].endswith(".txt"):
             raise Exception("File to save to must be a text file!")
-        
+
         write_bool = 1
 
-    with open(args[0], encoding="utf-8") as file:
+    # Error message if file does not exist
+    try:
+        f = open(args[1])
+        f.close()
+    except FileNotFoundError:
+        raise Exception("ERROR: This file does not exist!")
+
+    with open(args[1], encoding="utf-8") as file:
         f = file.read()
         
+        # Uncomment this to create and save the word count dict
+        get_most_frequent_words(f, save=True)
+
         if write_bool:
-            text_summary(f, file=args[1])
+            text_summary(f, output_file=args[2])
         else:
             text_summary(f)
